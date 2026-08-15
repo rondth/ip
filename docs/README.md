1493b0c53e3fcb03831a607597a146128a1bf183
eron <erondathan@gmail.com>
2026-08-16 06:32:56 +0800

    Change Duke to Henry

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Henry User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Henry.java
@@ -0,0 +1,14 @@
+/**
+ * Starts the Henry chatbot application.
+ */
+public class Henry {
+    public static void main(String[] args) {
+        String banner = " _   _                      \n"
+                + "| | | | ___ _ __  _ __ _   _\n"
+                + "| |_| |/ _ \\ '_ \\| '__| | | |\n"
+                + "|  _  |  __/ | | | |  | |_| |\n"
+                + "|_| |_|\\___|_| |_|_|   \\__, |\n"
+                + "                         |___/ \n";
+        System.out.println(banner);
+    }
+}
