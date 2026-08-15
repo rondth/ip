7700bc042a73839ed78357776b53454390d5cf84
eron <erondathan@gmail.com>
2026-08-16 06:37:11 +0800

    Add greeting and exit

--- src/main/java/Henry.java
+++ src/main/java/Henry.java
@@ -3,12 +3,20 @@
  */
 public class Henry {
     public static void main(String[] args) {
+        String separator = "____________________________________________________________";
         String banner = " _   _                      \n"
                 + "| | | | ___ _ __  _ __ _   _\n"
                 + "| |_| |/ _ \\ '_ \\| '__| | | |\n"
                 + "|  _  |  __/ | | | |  | |_| |\n"
                 + "|_| |_|\\___|_| |_|_|   \\__, |\n"
                 + "                       |___/ \n";
-        System.out.println(banner);
+
+        System.out.println(separator);
+        System.out.print(banner);
+        System.out.println("Hello! I'm Henry.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
