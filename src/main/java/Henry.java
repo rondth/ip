58c564b74b7ed3dbb0df2569667d0ce3d6041b23
eron <erondathan@gmail.com>
2026-08-16 06:52:15 +0800

    Add echo

--- src/main/java/Henry.java
+++ src/main/java/Henry.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 /**
  * Starts the Henry chatbot application.
  */
 public class Henry {
+    /**
+     * Greets the user, echoes each command, and exits when the user enters bye.
+     *
+     * @param args command-line arguments; not used
+     */
     public static void main(String[] args) {
         String separator = "____________________________________________________________";
         String banner = " _   _                      \n"
@@ -16,7 +23,18 @@ public class Henry {
         System.out.println("Hello! I'm Henry.");
         System.out.println("What can I do for you?");
         System.out.println(separator);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(separator);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(separator);
+                break;
+            }
+
+            System.out.println(command);
+            System.out.println(separator);
+        }
     }
 }
